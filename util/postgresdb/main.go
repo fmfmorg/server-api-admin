82d687011cee98b0da8f28c874a07cd7d5f1327d
fmfm2024 <fairymade2024@gmail.com>
2024-11-23 21:16:03 +0000

    .

--- util/postgresdb/main.go
+++ util/postgresdb/main.go
@@ -14,12 +14,6 @@ var (
 )
 
 func init() {
-	fmt.Println("host: ", config.DBHost)
-	fmt.Println("port: ", config.DBPort)
-	fmt.Println("password: ", config.DBPassword)
-	fmt.Println("username: ", config.DBUsername)
-	fmt.Println("db name: ", config.DBName)
-	fmt.Println("sslmode: ", config.DBSslMode)
 	var err error
 	DB, err = sql.Open("postgres", fmt.Sprintf("host=%s port=%s password=%s user=%s dbname=%s sslmode=%s", config.DBHost, config.DBPort, config.DBPassword, config.DBUsername, config.DBName, config.DBSslMode))
 	if err != nil {
